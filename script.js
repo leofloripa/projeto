9a878cd287417095033ca319250a4d497156afff
leofloripa <leonardofloripa@gmail.com>
2023-02-13 11:14:48 -0300

    add new content

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let sliderElement = document.querySelector(#slider);
+let buttonElement = document.querySelector(#button);
+
+let sizePassword = document.querySelector(#valor);
+let password = document.querySelector(#passord);
+
+let containerPassord = document.querySelector(#container-password);
+
+let charset = "ABCDEFGHIJKLMNOPQRSTUVXWYZabcdefghijklmnopqrstuvwxyz0123456789";
+let novasenha = "";
+
